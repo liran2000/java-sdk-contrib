7b7c1425c1a9526633ffccc4b8655debbde10433
liran2000 <liran2000@gmail.com>
2023-07-25 12:50:20 +0300

    feat: ISSUE-658 go-feature-flag sdk - add provider cache and publish data collector events
    Signed-off-by: liran2000 <liran2000@gmail.com>

---
+++ providers/go-feature-flag/src/main/java/dev/openfeature/contrib/providers/gofeatureflag/events/Events.java
@@ -0,0 +1,24 @@
+package dev.openfeature.contrib.providers.gofeatureflag.events;
+
+import lombok.Getter;
+
+import java.util.*;
+
+/**
+ * Events data.
+ */
+@Getter
+public class Events {
+    private List<Event> events = new LinkedList<>();
+
+    private static Map<String, String> meta = new HashMap<>();
+
+    static {
+        meta.put("provider", "java");
+        meta.put("openfeature", "true");
+    }
+
+    public Events(List<Event> events) {
+        this.events = events;
+    }
+}
